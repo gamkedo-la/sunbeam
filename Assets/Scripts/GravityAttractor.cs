fa6fe9333b81c8c522b8d7f859b4e75750cb5afe
TimWaskett <tim.waskett@gmail.com>
2016-10-25 21:40:49 +0100

    First commit

---
+++ Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityAttractor : MonoBehaviour
+{
+    [SerializeField] float m_gravity = -10f;
+
+
+    public void Attract(Rigidbody rigidbody)
+    {
+        var bodyTransform = rigidbody.transform;
+
+        var targetDirection = (bodyTransform.position - transform.position).normalized;
+
+        if (rigidbody.freezeRotation)
+        {
+            var bodyUp = bodyTransform.up;
+
+            bodyTransform.rotation = Quaternion.FromToRotation(bodyUp, targetDirection) * bodyTransform.rotation;
+        }
+
+        rigidbody.AddForce(targetDirection * m_gravity);
+    }
+}
---
+++ Assets/Scripts/GravityBody.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    private GravityAttractor m_gravityAttractor;
+    private Rigidbody m_rigidBody;
+
+
+    void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+        m_rigidBody.useGravity = false;
+
+        m_gravityAttractor = GameObject.FindGameObjectWithTag(Tags.Planet).GetComponent<GravityAttractor>();
+    }
+
+    
+	void FixedUpdate()
+    {
+        m_gravityAttractor.Attract(m_rigidBody);
+	}
+}
