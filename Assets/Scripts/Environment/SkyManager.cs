9f5bccdcd6164eb685bea79d1937c87516adcf7d
TimWaskett <tim.waskett@gmail.com>
2016-10-26 13:44:29 +0100

    FirstPersonController and SkyManager more or less working as intended

---
+++ Assets/Scripts/Environment/SkyManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class SkyManager : MonoBehaviour
+{
+    [SerializeField] Gradient m_skyColour;
+    [SerializeField] Gradient m_sunColour;
+    [SerializeField] AnimationCurve m_sunIntensity;
+    [SerializeField] AnimationCurve m_ambientIntensity;
+
+    private Transform m_player;
+    private Transform m_sunImage;
+    private Light m_sunLight;
+    private Material m_skybox;
+    private Material m_sunMaterial;
+    private Color m_originalSkyboxColour;
+    private Color m_originalSunColour;
+    private float m_sunDistanceFromPlayer;
+
+
+	void Start()
+    {
+        m_player = GameObject.FindGameObjectWithTag(Tags.Player).transform;
+        m_sunLight = GetComponent<Light>();
+
+        var sunMesh = GetComponentInChildren<MeshRenderer>();
+        m_sunImage = sunMesh.transform;
+        m_sunMaterial = sunMesh.material;
+
+        m_skybox = RenderSettings.skybox;
+
+        m_originalSkyboxColour = m_skybox.GetColor("_Tint");
+        m_originalSunColour = m_sunMaterial.GetColor("_TintColor");
+
+        m_sunDistanceFromPlayer = Vector3.Distance(m_player.position, m_sunImage.position);
+	}
+	
+
+	void Update()
+    {
+        var playerDirection = m_player.position.normalized;
+        float dotToPlayer = Vector3.Dot(-transform.forward, playerDirection);
+
+        float evaluationValue = 0.5f * (dotToPlayer + 1f);
+
+        var skyColour = m_skyColour.Evaluate(evaluationValue);
+        m_skybox.SetColor("_Tint", skyColour);
+
+        RenderSettings.ambientIntensity = m_ambientIntensity.Evaluate(evaluationValue);
+        m_sunLight.intensity = m_sunIntensity.Evaluate(evaluationValue);
+        var sunColour = m_sunColour.Evaluate(evaluationValue);
+        m_sunLight.color = sunColour;
+        m_sunMaterial.SetColor("_TintColor", sunColour);
+
+        m_sunImage.position = m_player.position - transform.forward * m_sunDistanceFromPlayer;
+	}
+
+
+    void OnDestroy()
+    {
+        m_skybox.SetColor("_Tint", m_originalSkyboxColour);
+        m_sunMaterial.SetColor("_TintColor", m_originalSunColour);
+    }
+}
---
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Look")]
+    [SerializeField] float m_mouseSensitivityX = 3f;
+    [SerializeField] float m_mouseSensitivityY = 3f;
+
+    [SerializeField] Vector2 m_verticalLookMinMan = new Vector2(-90f, 90f);
+
+    [Header("Walk")]
+    [SerializeField] float m_walkSpeed = 3f;
+    [SerializeField] float m_runSpeed = 10f;
+    [SerializeField] float m_moveSmooth = 0.15f;
+    [SerializeField] bool m_toggleRun = true;
+
+    [Header("Jump")]
+    [SerializeField] float m_jumpForce = 5f;
+    [SerializeField] LayerMask m_jumpMask;
+    [SerializeField] float m_groundedRayDistance = 0.1f;
+
+
+    private Rigidbody m_rigidbody;
+    private Transform m_camera;
+    private float m_verticalLookRotation;
+    private Vector3 m_moveAmount;
+    private Vector3 m_smoothMoveVelocity;
+    private bool m_grounded;
+    private bool m_isRunning;
+
+
+	void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_camera = Camera.main.transform;
+
+        StartCoroutine(GetSprint());
+    }
+	
+
+	void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(Vector3.up * mouseX * m_mouseSensitivityX);
+        m_verticalLookRotation += mouseY * m_mouseSensitivityY;
+        m_verticalLookRotation = Mathf.Clamp(m_verticalLookRotation, m_verticalLookMinMan.x, m_verticalLookMinMan.y);
+        m_camera.localEulerAngles = Vector3.left * m_verticalLookRotation;
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        var moveDirection = new Vector3(h, 0, v).normalized;
+        float speed = m_isRunning ? m_runSpeed : m_walkSpeed;
+        var targetMoveAmount = moveDirection * speed;
+        m_moveAmount = Vector3.SmoothDamp(m_moveAmount, targetMoveAmount, ref m_smoothMoveVelocity, m_moveSmooth);
+
+        if (m_grounded && Input.GetButtonDown("Jump"))
+        {
+            m_rigidbody.AddForce(transform.up * m_jumpForce, ForceMode.Impulse);
+        }
+
+        m_grounded = false;
+        var ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, m_groundedRayDistance, m_jumpMask))
+        {
+            m_grounded = true;
+        }
+	}
+
+
+    void FixedUpdate()
+    {
+        m_rigidbody.MovePosition(m_rigidbody.position + transform.TransformDirection(m_moveAmount) * Time.deltaTime);
+    }
+
+
+    private IEnumerator GetSprint()
+    {
+        bool buttonPressedPreviously = false;
+
+        while (true)
+        {
+            bool buttonPressed = Input.GetAxisRaw("Sprint") == 1f;
+
+            if (!m_toggleRun)
+                m_isRunning = buttonPressed;
+            else
+            {
+                if (buttonPressed && !buttonPressedPreviously)
+                    m_isRunning = !m_isRunning;
+
+                buttonPressedPreviously = buttonPressed;
+            }
+
+            yield return null;
+        }
+    }
+}
