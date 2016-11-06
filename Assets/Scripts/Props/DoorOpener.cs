ae1dc9a615b4abacb7463f90b34ee76a19b5be9f
TimWaskett <tim.waskett@gmail.com>
2016-11-06 12:07:43 +0000

    Added animated door to test scene

---
+++ Assets/Scripts/Props/DoorOpener.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class DoorOpener : MonoBehaviour
+{
+    private Animator m_animator;
+
+
+    void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+
+    public void OpenDoor()
+    {
+        m_animator.SetTrigger("Open");
+    }
+}
