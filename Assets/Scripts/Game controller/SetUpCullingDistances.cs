882f2555806443de36ab354ae6f0b323cdff9e09
TimWaskett <tim.waskett@gmail.com>
2016-11-23 18:37:34 +0000

    Set culling distance for grass to 30m.

---
+++ Assets/Scripts/Game controller/SetUpCullingDistances.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetUpCullingDistances : MonoBehaviour
+{
+	void Start() 
+	{
+		var camera = Camera.main;
+		float[] distances = new float[32];
+
+		distances[10] = 30f;		// grass
+
+		camera.layerCullDistances = distances;
+		camera.layerCullSpherical = true;
+	}
+}
