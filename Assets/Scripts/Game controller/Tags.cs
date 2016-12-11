90d87d9111a915a5978377703a6232e6acf949e0
TimWaskett <tim.waskett@gmail.com>
2016-12-11 16:46:45 +0000

    Added moving platform to wooden tower

--- Assets/Scripts/Game controller/Tags.cs
+++ Assets/Scripts/Game controller/Tags.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Tags : MonoBehaviour
 {
     public const string Player = "Player";
+    public const string PlayerBody = "Player body";
     public const string Planet = "Planet";
     public const string Environment = "Environment";
     public const string Sky = "Sky";
---
+++ Assets/Scripts/Structures/WoodenTowerPlatformController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class WoodenTowerPlatformController : MonoBehaviour
+{
+    private Animator m_anim;
+    private int m_up;
+    private int m_down;
+
+	
+    void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        //m_up = 
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(Tags.PlayerBody))
+            return;
+
+        // TODO: determine state of animator before triggering correct animation
+
+        print("Triggered by " + other.name);
+        m_anim.SetTrigger("Up");
+    }
+}
