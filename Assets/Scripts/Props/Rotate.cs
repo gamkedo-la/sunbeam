d955f5c511171bf3819db9c4af450ef3c672eb06
TimWaskett <tim.waskett@gmail.com>
2016-12-18 09:21:42 +0000

    Added Gear 12

---
+++ Assets/Scripts/Props/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] float m_rotationRate = 10f;
+	
+	void Update()
+    {
+        transform.Rotate(Vector3.forward, Time.deltaTime * m_rotationRate);
+	}
+}
