f5e2a3c082678fa14bb1a268fe0be400ba97ec60
TimWaskett <tim.waskett@gmail.com>
2017-01-14 14:29:58 +0000

    Added the start of the tutorial puzzle at the North pole.

--- Assets/Scripts/Game controller/CheatControls.cs
+++ Assets/Scripts/Game controller/CheatControls.cs
@@ -9,12 +9,14 @@ public class CheatControls : MonoBehaviour
     [SerializeField] Transform[] m_spawnPoints;
 
     private Transform m_player;
+    private Rigidbody m_playerRigidbody;
     private Transform m_sky;
 
 
     void Awake()
     {
         m_player = GameObject.FindGameObjectWithTag(Tags.Player).transform;
+        m_playerRigidbody = m_player.GetComponent<Rigidbody>();
         m_sky = GameObject.FindGameObjectWithTag(Tags.Sky).transform;
     }
 
@@ -26,61 +28,73 @@ public class CheatControls : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Alpha1) && m_spawnPoints.Length > 0)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[0].position;
             m_player.rotation = m_spawnPoints[0].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2) && m_spawnPoints.Length > 1)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[1].position;
             m_player.rotation = m_spawnPoints[1].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3) && m_spawnPoints.Length > 2)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[2].position;
             m_player.rotation = m_spawnPoints[2].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha4) && m_spawnPoints.Length > 3)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[3].position;
             m_player.rotation = m_spawnPoints[3].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha5) && m_spawnPoints.Length > 4)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[4].position;
             m_player.rotation = m_spawnPoints[4].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha6) && m_spawnPoints.Length > 5)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[5].position;
             m_player.rotation = m_spawnPoints[5].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha7) && m_spawnPoints.Length > 6)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[6].position;
             m_player.rotation = m_spawnPoints[6].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha8) && m_spawnPoints.Length > 7)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[7].position;
             m_player.rotation = m_spawnPoints[7].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha9) && m_spawnPoints.Length > 8)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[8].position;
             m_player.rotation = m_spawnPoints[8].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.Alpha0) && m_spawnPoints.Length > 9)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[9].position;
             m_player.rotation = m_spawnPoints[9].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.LeftBracket) && m_spawnPoints.Length > 10)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[10].position;
             m_player.rotation = m_spawnPoints[10].rotation;
         }
         else if (Input.GetKeyDown(KeyCode.RightBracket) && m_spawnPoints.Length > 11)
         {
+            m_playerRigidbody.velocity = Vector3.zero;
             m_player.position = m_spawnPoints[11].position;
             m_player.rotation = m_spawnPoints[11].rotation;
         }
