5fd5f97e29d5e2c0992b41e693e11c9c321a65d2
TimWaskett <tim.waskett@gmail.com>
2017-01-17 08:22:00 +0000

    Added the ability for solar panels to be charged by direct sunlight.

---
+++ Assets/Scripts/Props/SolarPanelSunDirectionChecker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarPanelSunDirectionChecker : MonoBehaviour
+{
+    [SerializeField] float m_sunDotThreshold = 0.7f;
+    [SerializeField] float m_sunRayDistance = 500f;
+
+    private SolarPanelManager m_solarPanelManager;
+    private Transform m_sun;
+
+
+	
+    void Awake()
+    {
+        m_solarPanelManager = GetComponentInParent<SolarPanelManager>();
+        var sky = GameObject.FindGameObjectWithTag(Tags.Sky);
+
+        if (sky != null)
+            m_sun = sky.GetComponent<Light>().transform;
+    }
+
+
+    void Update()
+    {
+        var direction = -m_sun.forward;
+        float dot = Vector3.Dot(direction, transform.forward);
+
+        if (dot < m_sunDotThreshold)
+            return;
+
+        RaycastHit hit;
+        var ray = new Ray(transform.position, direction);
+
+        if (Physics.Raycast(ray, out hit, m_sunRayDistance))
+        {
+            float distance = Vector3.Distance(transform.position, hit.point);
+
+            Debug.DrawRay(transform.position, direction * distance, Color.red);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, direction * m_sunRayDistance, Color.green);
+            m_solarPanelManager.ChargeUp();
+        }  
+    }
+}
