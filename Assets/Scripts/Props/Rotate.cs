3e82488bfa044bfcedc237ad783409e106383593
TimWaskett <tim.waskett@gmail.com>
2017-02-12 14:03:00 +0000

    Added first puzzle to biome 6

--- Assets/Scripts/Props/Rotate.cs
+++ Assets/Scripts/Props/Rotate.cs
@@ -2,12 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class Rotate : MonoBehaviour, IActivatable
 {
+    [SerializeField] bool m_startActive;
     [SerializeField] float m_rotationRate = 10f;
-	
+    [SerializeField] float m_accelerationRate = 1f;
+
+
+    private float m_currentRotationRate;
+    private bool m_active;
+
+
+    void Awake()
+    {
+        if (m_startActive)
+        {
+            m_active = true;
+            m_currentRotationRate = m_rotationRate;
+        }
+    }
+
+
 	void Update()
     {
-        transform.Rotate(Vector3.up, Time.deltaTime * m_rotationRate);
+        if (!m_active)
+            return;
+
+        transform.Rotate(Vector3.up, Time.deltaTime * m_currentRotationRate);
 	}
+
+
+    public void Activate()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Accelerate());
+    }
+
+
+    public void Deactivate()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Decelerate());
+    }
+
+
+    private IEnumerator Accelerate()
+    {
+        m_active = true;
+
+        while (m_currentRotationRate < m_rotationRate)
+        {
+            m_currentRotationRate += Time.deltaTime * m_accelerationRate;
+            yield return null;
+        }
+
+        m_currentRotationRate = m_rotationRate;
+    }
+
+
+    private IEnumerator Decelerate()
+    {
+        m_active = true;
+
+        while (m_currentRotationRate > 0)
+        {
+            m_currentRotationRate -= Time.deltaTime * m_accelerationRate;
+            yield return null;
+        }
+
+        m_currentRotationRate = 0;
+        m_active = false;
+    }
 }
