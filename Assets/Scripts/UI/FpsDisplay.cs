eafead26a0fb56009809baefb81f714d6613520a
TimWaskett <tim.waskett@gmail.com>
2017-02-13 08:28:11 +0000

    Added FPS display and log for mouse inputs to see what's going on when the mouse controls stop working.

---
+++ Assets/Scripts/UI/FpsDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class FpsDisplay : MonoBehaviour
+{
+    [SerializeField] float m_calculationTime = 0.5f;
+
+    private Text m_text;
+	
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+    }
+
+
+    void Start()
+    {
+        if (GameController.AllowCheatMode)
+            StartCoroutine(ShowFps());
+        else
+            m_text.text = "";
+    }
+
+
+    IEnumerator ShowFps()
+    {
+        int frames = 0;
+        float time = 0;
+
+        while (true)
+        {
+            time += Time.unscaledDeltaTime;
+            frames++;
+
+            if (time > m_calculationTime)
+            {
+                float fps = frames / time;
+
+                m_text.text = string.Format("{0:00}", fps);
+
+                frames = 0;
+                time = 0;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/LogDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class LogDisplay : MonoBehaviour
+{
+    private Text m_text;
+
+
+    void Awake()
+    {
+        m_text = GetComponent<Text>();
+
+        if (!GameController.AllowCheatMode)
+            m_text.text = "";
+    }
+	
+
+	void Update()
+    {
+        if (!GameController.AllowCheatMode)
+            return;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        m_text.text = string.Format("Mouse: [{0,5:0.00;-0.00}, {1,5:0.00;-0.00}]", mouseX, mouseY);
+    }
+}
