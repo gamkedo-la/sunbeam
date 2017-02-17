da28819d3f76c281d8ebef08a3f2c06402071a13
TimWaskett <tim.waskett@gmail.com>
2017-02-17 12:07:29 +0000

    Added sfx to the solar panels.

--- Assets/Scripts/Props/SolarPanelManager.cs
+++ Assets/Scripts/Props/SolarPanelManager.cs
@@ -14,7 +14,14 @@ public class SolarPanelManager : MonoBehaviour
     [SerializeField] UnityEvent m_triggerActions;
     [SerializeField] UnityEvent m_dischargedTriggerActions;
 
+    [Header("Audio")]
+    [SerializeField] AudioSource m_audioSource;
+    [SerializeField] AudioClip m_chargingUpClip;
+    [SerializeField] AudioClip m_poweringDownClip;
+    [SerializeField] AudioClip m_chargedClip;
+
     private float m_chargeLevel;
+    private float m_previousChargeLevel;
     private bool m_charging;
     private bool m_charged;
     private bool m_discharged;
@@ -39,14 +46,24 @@ public class SolarPanelManager : MonoBehaviour
             m_discharged = true;
         }
 
+        m_previousChargeLevel = m_chargeLevel;
+
         UpdateChargeLevel();    
     }
 
 
     void Update()
     {
+        if (m_charging)
+        {
+            Charge();
+            m_charging = false;
+        }
+
         if (m_discharges)
             Discharge();
+
+        m_previousChargeLevel = m_chargeLevel;
     }
 
 
@@ -55,6 +72,12 @@ public class SolarPanelManager : MonoBehaviour
         if (m_charged && (!m_discharges || !m_dischargesOnceCharged))
             return;
 
+        m_charging = true;
+    }
+
+
+    private void Charge()
+    {
         m_chargeLevel += Time.deltaTime / m_chargeTime;
 
         if (m_discharges)
@@ -75,7 +98,7 @@ public class SolarPanelManager : MonoBehaviour
     }
 
 
-    public void Discharge()
+    private void Discharge()
     {
         if (m_charged && (!m_discharges || !m_dischargesOnceCharged))
             return;
@@ -106,12 +129,60 @@ public class SolarPanelManager : MonoBehaviour
             m_chargedButton.SetActive(true);
         else if (m_chargeLevel <= 0f && m_chargedButton.activeSelf == true)
             m_chargedButton.SetActive(false);
+
+        if (m_audioSource != null)
+        {
+            if (m_chargeLevel > m_previousChargeLevel)
+            {
+                if (m_audioSource.clip != m_chargingUpClip)
+                {
+                    m_audioSource.Stop();
+                    //print("Stop to switch clip for charge up");
+                    m_audioSource.clip = m_chargingUpClip;
+                }
+
+                if (!m_audioSource.isPlaying)
+                {
+                    //print("Play charge up");
+                    m_audioSource.Play();
+                }
+            }
+            else if (m_chargeLevel < m_previousChargeLevel)
+            {
+                if (m_audioSource.clip != m_poweringDownClip)
+                {
+                    m_audioSource.Stop();
+                    //print("Stop to switch clip for power down");
+                    m_audioSource.clip = m_poweringDownClip;
+                }
+
+                if (!m_audioSource.isPlaying)
+                {
+                    //print("Play power down");
+                    m_audioSource.Play();
+                }
+            }
+
+            if (m_audioSource.isPlaying && m_chargeLevel == 0)
+            {
+                //print("Stop because fully discharged");
+                m_audioSource.Stop();
+            }
+        }
     }
 
 
     private void TriggerActions()
     {
         m_triggerActions.Invoke();
+
+        if (m_audioSource != null)
+        {
+            m_audioSource.loop = true;
+            m_audioSource.clip = m_chargedClip;
+            m_audioSource.pitch = 1.0f;
+            m_audioSource.Play();
+        }
 
         m_chargedActionsTrggered = true;
         m_dischargedActionsTriggered = false;
