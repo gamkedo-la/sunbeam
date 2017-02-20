d0b371de5a285c680676b0bebdc39e856ccd5402
TimWaskett <tim.waskett@gmail.com>
2017-02-20 08:27:03 +0000

    Slight adjustment to the solar panel audio levels.

--- Assets/Scripts/Props/SolarPanelManager.cs
+++ Assets/Scripts/Props/SolarPanelManager.cs
@@ -19,8 +19,10 @@ public class SolarPanelManager : MonoBehaviour
     [SerializeField] AudioSource m_chargingUpAudio;
     [SerializeField] AudioSource m_poweringDownAudio;
     [SerializeField] AudioSource m_actionsTriggeredAudio;
+    //[SerializeField] float m_actionsTriggeredDistanceThreshold = 40f;
     [SerializeField] AudioSource m_chargedAudio;
 
+    private Transform m_camera;
     private float m_chargeLevel;
     private float m_previousChargeLevel;
     private bool m_charging;
@@ -32,6 +34,8 @@ public class SolarPanelManager : MonoBehaviour
 
     void Awake()
     {
+        m_camera = Camera.main.transform;
+
         if (m_startCharged)
         {
             m_chargeLevel = 1f;
@@ -179,7 +183,12 @@ public class SolarPanelManager : MonoBehaviour
         m_triggerActions.Invoke();
 
         if (m_actionsTriggeredAudio != null)
-            m_actionsTriggeredAudio.Play();
+        {
+            //float distance = Vector3.Distance(m_actionsTriggeredAudio.transform.position, m_camera.position);
+            //print(distance);
+            //if (distance < m_actionsTriggeredDistanceThreshold)
+                m_actionsTriggeredAudio.Play();
+        }
 
         if (m_chargedAudio != null)
             m_chargedAudio.Play();
