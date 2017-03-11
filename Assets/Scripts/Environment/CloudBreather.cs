faa9657c3bc2b8bf88cae6ed3705962a465f54e1
TimWaskett <tim.waskett@gmail.com>
2017-03-11 21:18:50 +0000

    Added a 'cloud breathing' script to the clouds to make them, well, 'breath'

---
+++ Assets/Scripts/Environment/CloudBreather.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBreather : MonoBehaviour
+{
+    private Vector2 m_scaleMultiplierMinMax = new Vector2(0.7f, 1.2f);
+    private float m_breathingRate = 0.05f;
+
+    private Vector3 m_initialScale;
+    private float m_perlinX;
+
+
+    void Awake()
+    {
+        m_initialScale = transform.localScale;
+        m_perlinX = Random.Range(0f, 100f);
+    }
+	
+
+	void Update()
+    {
+        float scale = Mathf.PerlinNoise(m_perlinX, Time.time * m_breathingRate);
+        scale = Mathf.Lerp(m_scaleMultiplierMinMax.x, m_scaleMultiplierMinMax.y, scale);
+
+        transform.localScale = scale * m_initialScale;
+    }
+}
