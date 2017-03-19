05681683ad50c232bcd943586ae8edd30de35d94
TimWaskett <tim.waskett@gmail.com>
2017-03-19 14:18:26 +0000

    Added player barriers to the arctic biome

---
+++ Assets/Scripts/Props/TurnOffRendererAtStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class TurnOffRendererAtStart : MonoBehaviour
+{
+    [SerializeField] bool m_active = true;
+
+    void Awake()
+    {
+        if (!m_active)
+            return;
+
+        var renderer = GetComponent<MeshRenderer>(); 
+        renderer.enabled = false;
+    }
+
+}
