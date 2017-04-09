cca3167b852a560f3a1985f633537dcf031dd7bf
TimWaskett <tim.waskett@gmail.com>
2017-04-09 15:35:07 +0100

    Added a tutorial puzzle to demonstrate the difference between the two types of solar panels.

--- Assets/Scripts/Props/DoorOpener.cs
+++ Assets/Scripts/Props/DoorOpener.cs
@@ -4,12 +4,17 @@ using System.Collections;
 [RequireComponent(typeof(Animator))]
 public class DoorOpener : MonoBehaviour
 {
+    [SerializeField] bool m_openOnStart;
+
     private Animator m_animator;
 
 
     void Awake()
     {
         m_animator = GetComponent<Animator>();
+
+        if (m_openOnStart)
+            OpenDoor();
     }
 
 
