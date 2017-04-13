543f536f817035d88b9fd492e78dfe722f7ddbd2
TimWaskett <tim.waskett@gmail.com>
2017-04-13 15:31:30 +0100

    Added camera FoV slider to the pause menu

---
+++ Assets/Scripts/UI/FieldOfViewSliderControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FieldOfViewSliderControl : MonoBehaviour
+{
+    private Camera m_camera;
+
+	
+    void Awake()
+    {
+        m_camera = Camera.main;
+    }
+
+
+    public void ChangeCameraFoV(float fov)
+    {
+        m_camera.fieldOfView = fov;
+    }
+}
